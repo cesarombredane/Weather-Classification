7bc3165790bf2cd83aee49021296719d22ab660b
cesarombredane <combredane@gmail.com>
2023-12-05 09:50:33 +0100

    optimisation

--- Weather_Classification_TP.py
+++ Weather_Classification_TP.py
@@ -1,5 +1,4 @@
 # Common imports
-from PIL import UnidentifiedImageError
 from tqdm import tqdm
 from glob import glob
 import numpy as np
@@ -20,79 +19,55 @@ import os
 # Categories
 class_names = {0: 'cloudy', 1: 'foggy', 2: 'rainy', 3: 'shine', 4: 'sunrise'}
 
-
 def load_image(path):
     '''
     Takes in path of the image and load it
     '''
-    img = resize(img_to_array(load_img(path)) / 255., (256, 256))
-    return img
-
+    return resize(img_to_array(load_img(path)) / 255., (256, 256))
 
 def load_data(img_paths):
     '''
     Takes in path of the images and loads all
     '''
     X = np.zeros(shape=(len(img_paths), 256, 256, 3))
-    for i, path in tqdm(enumerate(img_paths), desc="Loading"):
-        X[i] = load_image(path)
-
+    for i, path in tqdm(enumerate(img_paths), desc='Loading'): X[i] = load_image(path)
     return X
 
 
 # Load images
 image_paths = [
     x for x in sorted(glob('./data/*.jpg'))
-    if imghdr.what(x) is not None or print(f"File processing ...\nError: The file '{x}' is not a valid image!")
+    if imghdr.what(x) is not None or print(f'error: file {x} is not a valid image')
 ]
-print(f"\nTotal number of valid images: {len(image_paths)}")
+print(f'number of valid images: {len(image_paths)}')
 
 # Check the existing processed files
 output_dir = './result/'
-existing_result_files = glob(os.path.join(output_dir, 'predictions_*.csv'))
 # Extract processed image names from existing CSV files
 processed_images = set()
-for result_file in existing_result_files:
+for result_file in glob(os.path.join(output_dir, 'predictions_*.csv')):
     with open(result_file, 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            image_name = row[0]
-            processed_images.add(image_name)
+        for row in csv.reader(f):
+            processed_images.add(row[0])
 
 # Filter already processed images
 unprocessed_image_paths = [path for path in image_paths if os.path.basename(path) not in processed_images]
-print(f"Total number of unprocessed valid images: {len(unprocessed_image_paths)}")
+print(f'number of unprocessed images: {len(unprocessed_image_paths)}')
 
-if len(unprocessed_image_paths) == 0:
-    print("\nAll valid images have been processed! The CSV sum-up is/are already in the result folder.")
+# Ask user if they want to skip processing already predicted images
+processed_to_write = unprocessed_image_paths if input('processing already predicted images? (Y/n): ') or 'Y' == 'Y' else image_paths
+if len(processed_to_write) == 0:
+    print('no images to process')
     exit()
-elif len(unprocessed_image_paths) == len(image_paths):
-    print("\nAll valid images in the data folder are unprocessed, so the prediction of all images will now begin...")
-
-    # Load all images for prediction
-    images = load_data(image_paths)
-    processed_to_write = image_paths
-else:
-    # Interactive prompt for user choice
-    skip_processed = input("\nDo you want to skip processing already predicted images? (yes/no): ").lower()
-
-    if skip_processed == 'yes':
-        # Load unprocessed images for prediction
-        images = load_data(unprocessed_image_paths)
-        processed_to_write = unprocessed_image_paths
-    else:
-        # Load all images for prediction (processed and unprocessed)
-        images = load_data(image_paths)
-        processed_to_write = image_paths
 
 # Load model
 model_v3 = load_model('./data/ResNet152V2-Weather-Classification-03.h5')
 
 # Make Predictions
-preds = np.argmax(model_v3.predict(images), axis=-1)
+preds = np.argmax(model_v3.predict(load_data(processed_to_write)), axis=-1)
 
 # Get timestamp
-timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
 # Generate CSV file name based on timestamp
 csv_file_path = f'{output_dir}predictions_{timestamp}.csv'
@@ -101,8 +76,6 @@ csv_file_path = f'{output_dir}predictions_{timestamp}.csv'
 with open(csv_file_path, mode='w', newline='') as file:  # 'a' mode for appending to an existing file
     writer = csv.writer(file)
     for i, image_path in enumerate(processed_to_write):
-        image_name = os.path.basename(image_path)
-        prediction_label = class_names[preds[i]]
-        writer.writerow([image_name, prediction_label])
+        writer.writerow([os.path.basename(image_path), class_names[preds[i]]])
 
-print("New CSV file based on your choice has been successfully generated!")
+print('csv file successfully generated')
